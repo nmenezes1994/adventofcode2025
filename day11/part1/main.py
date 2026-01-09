f3d9c3d8bce03840db9dde69bd89cbb78a81e665
Nicolas Menezes <menezes.nicolas@yahoo.com>
2026-01-09 15:39:39 -0300

    refactor: improve day 11 part 1 performance

--- day11/part1/main.py
+++ day11/part1/main.py
@@ -1,12 +1,12 @@
 import sys
 
 ARGS_FILE_NAME_INDEX: int = 1
-STARTING_DEVICE_NAME: str = 'you'
+STARTING_DEVICE_NAME: str = 'svr'
 REACTOR_DEVICE_NAME: str = 'out'
 
 def main(args: list[str]) -> None:
     connections = get_connections(args[ARGS_FILE_NAME_INDEX])
-    result = count_paths_to_reactor(connections, STARTING_DEVICE_NAME)
+    result = count_paths_to_reactor(connections, STARTING_DEVICE_NAME, {})
     print(f'The result is {result}')
 
 def get_connections(file_name: str) -> dict[str, list[str]]:
@@ -22,14 +22,18 @@ def get_connections(file_name: str) -> dict[str, list[str]]:
 
     return connections
 
-def count_paths_to_reactor(connections: dict[str, list[str]], current_device: str) -> int:
-    outputs = connections[current_device]
-    if len(outputs) == 1 and outputs[0] == REACTOR_DEVICE_NAME:
+def count_paths_to_reactor(connections: dict[str, list[str]], current_device: str, known_paths: dict[str, int]) -> int:
+    if current_device in known_paths:
+        return known_paths[current_device]
+    
+    if current_device not in connections:
+        known_paths[current_device] = 1
         return 1
     
     paths: int = 0
-    for output in outputs:
-        paths += count_paths_to_reactor(connections, output)
+    for output in connections[current_device]:
+        paths += count_paths_to_reactor(connections, output, known_paths)
+    known_paths[current_device] = paths
     return paths
 
 if __name__ == '__main__':
